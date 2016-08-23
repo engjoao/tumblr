c6ce0d02368d9dd47f9c088a6d31c99090083e63
engjoao <aquinosilva@gmail.com>
2016-08-23 10:05:42 -0300

    Add devise and Users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
   	@posts = Post.all.order('created_at DESC')
@@ -13,9 +14,9 @@ class PostsController < ApplicationController
 
   	if @post.save
   	  redirect_to @post
-	else
-	  render 'new'
-	end
+    else
+	   render 'new'
+    end
   end
 
   def show
